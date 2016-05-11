71991fceb950e2cd4c0775b832466660f2fface7
Jack.Yj.Din <jack.yj.din@benq.com>
2016-05-11 19:46:42 +0800

    搞定

--- HW10/FirstViewController.swift
+++ HW10/FirstViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
+enum GirlType {
+    case one,two,three
+}
+
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var firstGF: UIButton!
+    @IBOutlet weak var secondGF: UIButton!
+    @IBOutlet weak var thirdGF: UIButton!
+    
+    var girlType:GirlType? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(didNameChanged), name: "MyGirlNameChanged", object: nil)
@@ -31,8 +41,17 @@ class FirstViewController: UIViewController {
     
     //MARK: - Notificaiton
     func didNameChanged(notification:NSNotification) -> Void {
-        
+
         let newName:String = notification.object as! String
+        
+        switch girlType! {
+        case .one:
+            self.firstGF.setTitle(newName, forState: .Normal)
+        case .two:
+            self.firstGF.setTitle(newName, forState: .Normal)
+        case .three:
+            self.firstGF.setTitle(newName, forState: .Normal)
+        }
         
     }
     
@@ -42,6 +61,7 @@ class FirstViewController: UIViewController {
         
         let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("SecondViewController") as! SecondViewController
         secondViewController.grilfriendType = .FirstType
+        girlType = .one
         self.navigationController?.pushViewController(secondViewController, animated: true)
     }
 
@@ -49,12 +69,14 @@ class FirstViewController: UIViewController {
     
         let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("SecondViewController") as! SecondViewController
         secondViewController.grilfriendType = .SecondType
+        girlType = .two
         self.navigationController?.pushViewController(secondViewController, animated: true)
     }
     @IBAction func ThirdGF(sender: UIButton) {
         
         let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("SecondViewController") as! SecondViewController
         secondViewController.grilfriendType = .ThirdType
+        girlType = .three
         self.navigationController?.pushViewController(secondViewController, animated: true)
     }
 }
